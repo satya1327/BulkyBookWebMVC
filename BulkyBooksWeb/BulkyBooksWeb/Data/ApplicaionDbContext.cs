558944e497c2a76a852365bc7cfaea0210fccbdf
Satyajit Das <satyajit.das@euromonitor.com>
2023-03-18 00:51:14 +0530

    created DBContext and injected connection string to program file

---
+++ BulkyBooksWeb/BulkyBooksWeb/Data/ApplicaionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BulkyBooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBooksWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> categories { get; set; }
+
+
+    }
+}
--- BulkyBooksWeb/BulkyBooksWeb/Program.cs
+++ BulkyBooksWeb/BulkyBooksWeb/Program.cs
@@ -1,7 +1,11 @@
+using BulkyBooksWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BulkyBookConnection")));
 
 var app = builder.Build();
 
